0c2c2e0ff1b98e4ba0b52a729bf1115a935ba890
fauzan07 <fauzanfalke@gmail.com>
2021-10-13 15:57:39 +0530

    updated lec 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {Component} from "react";
 
 class App extends Component{
-    // render(){
-    //     return <>
-    //     <h1>App Component</h1>
-    //     <h3>App Component</h3>
-    //     </>
-    // }
+    render(){
+        return <>
+        <h1>App Component</h1>
+        <h3>App Component</h3>
+        </>
+    }
 
-    //fragment
+    //------------fragment-----------------
     // render(){
     //     return (
     //     <>
@@ -18,7 +18,7 @@ class App extends Component{
     //     )
     // }
 
-    //need closing tag
+    //-------------need closing tag-----------
     // render(){
     //     return (
     //     <>
@@ -29,7 +29,7 @@ class App extends Component{
     //     )
     // }
 
-    //how to use js in jsx syntax
+    //-------------how to use js in jsx syntax---------
     // render(){
     //     const num1 = 1;
     //     const num2 = 2;
@@ -45,7 +45,7 @@ class App extends Component{
     //         )
     //     }
 
-    //how to call functions in class component & jsx
+    //---------------how to call functions in class component & jsx-------------
     // add(num1, num2){
     //     return num1 + num2;
     // }
@@ -66,28 +66,138 @@ class App extends Component{
     //         )
     //     }
 
-    //how to use object call in jsx
-        add(num1, num2){
-            return num1 + num2;
-        }
+    //----------------how to use object call in jsx----------------------
+    //     add(num1, num2){
+    //         return num1 + num2;
+    //     }
 
-      render(){
-        const num1 = 1;
-        const num2 = 2;
-        const someObj = {name : "Salim", age : "25"}
-             return (
-            <>
-                <h1>App Component</h1>
-                <h3>testing js code</h3>
-                <span>{1 + 1}</span><br/>
-                <span>{num1 + num2}</span>
-                <h2>{new Date().toLocaleString()}</h2>
-                <h2>{this.add(1,1)}</h2>
-                <h1>My Name is {someObj.name}, my age is {someObj.age}</h1>
+    //   render(){
+    //     const num1 = 1;
+    //     const num2 = 2;
+    //     const someObj = {name : "Salim", age : "25"}
+    //          return (
+    //         <>
+    //             <h1>App Component</h1>
+    //             <h3>testing js code</h3>
+    //             <span>{1 + 1}</span><br/>
+    //             <span>{num1 + num2}</span>
+    //             <h2>{new Date().toLocaleString()}</h2>
+    //             <h2>{this.add(1,1)}</h2>
+    //         </>
+    //         )
+    //     }
 
-            </>
-            )
-        }
+    //---------------conditional expresion in react jsx------------------
+    //     add(num1, num2){
+    //         return num1 + num2;
+    //     }
+
+    //   render(){
+    //     const num1 = 1;
+    //     const num2 = 2;
+    //     const someObj = {name : "Salim", age : "25"}
+    //          return (
+    //         <>
+    //             <h1>App Component</h1>
+    //             <h3>testing js code</h3>
+    //             <span>{1 + 1}</span><br/>
+    //             <span>{num1 + num2}</span>
+    //             <h2>{new Date().toLocaleString()}</h2>
+    //             <h2>{this.add(1,1)}</h2>
+    //             <h1>My Name is {someObj.name}, my age is {someObj.age}</h1>
+    //             {3 > 2 ? "3 is bigger than 2" : "its not"}
+    //             {true && <h1>Thing i want to render</h1>}
+    //         </>
+    //         )
+    //     }
+
+    //------------------return jsx in jsx via function-------------------------
+    //     add(num1, num2){
+    //         return num1 + num2;
+    //     }
+
+    //     someJSX(){
+    //         return (
+    //             <>
+    //                 <ul>
+    //                     <li>Jsx List 1</li>
+    //                     <li>Jsx List 2</li>
+    //                     <li>Jsx List 3</li>
+    //                 </ul>
+    //             </>
+    //         )
+    //     }
+
+    //   render(){
+    //     const num1 = 1;
+    //     const num2 = 2;
+    //     const someObj = {name : "Salim", age : "25"}
+    //          return (
+    //         <>
+    //             <h1>App Component</h1>
+    //             <h3>testing js code</h3>
+    //             <span>{1 + 1}</span><br/>
+    //             <span>{num1 + num2}</span>
+    //             <h2>{new Date().toLocaleString()}</h2>
+    //             <h2>{this.add(1,1)}</h2>
+    //             <h1>My Name is {someObj.name}, my age is {someObj.age}</h1>
+    //             {3 > 2 ? "3 is bigger than 2" : "its not"}
+    //             {true && <h1>Thing i want to render</h1>}
+    //             {this.someJSX()}
+    //         </>
+    //         )
+    //     }
+
+    //------------------conditional rendering in jsx--------------------------
+    //     add(num1, num2){
+    //         return num1 + num2;
+    //     }
+
+    //     someJSX(){
+    //         return (
+    //             <>
+    //                 <ul>
+    //                     <li>Jsx List 1</li>
+    //                     <li>Jsx List 2</li>
+    //                     <li>Jsx List 3</li>
+    //                 </ul>
+    //             </>
+    //         )
+    //     }
+
+    //     condtionalRendering(){
+    //         if(3 < 0){
+    //             return(
+    //                <h1>3 is greater than 2</h1>
+    //             )
+    //         }else{
+    //             return(
+    //                 <h1>3 is less than 2</h1>
+    //              )
+    //         }
+    //     }
+
+    //   render(){
+    //     const num1 = 1;
+    //     const num2 = 2;
+    //     const someObj = {name : "Salim", age : "25"}
+    //          return (
+    //         <>
+    //             <h1>App Component</h1>
+    //             <h3>testing js code</h3>
+    //             <span>{1 + 1}</span><br/>
+    //             <span>{num1 + num2}</span>
+    //             <h2>{new Date().toLocaleString()}</h2>
+    //             <h2>{this.add(1,1)}</h2>
+    //             <h1>My Name is {someObj.name}, my age is {someObj.age}</h1>
+    //             {3 > 2 ? "3 is bigger than 2" : "its not"}
+    //             {true && <h1>Thing i want to render</h1>}
+    //             {this.someJSX()}
+    //             {this.condtionalRendering()}
+    //         </>
+    //         )
+    //     }
+
 }
 
 export default App;
